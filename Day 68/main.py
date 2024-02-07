47d53cf8cdd7377ea116f04d2a931b42cfa5f7d7
Vedant Kanoje <141636261+CodeSageVedx@users.noreply.github.com>
2024-02-07 23:49:27 +0530

    Create main.py

---
+++ Day 68/main.py
@@ -0,0 +1,16 @@
+#Plus One
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        # List -> Number
+        n = 0
+        for ele in digits:
+            n = (n*10) + ele
+        
+        n = n+1
+        
+        # Number -> List
+        digits = []
+        while n > 0:
+            digits.insert(0, n % 10)  
+            n //= 10 
+        return digits
