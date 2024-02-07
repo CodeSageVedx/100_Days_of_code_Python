4b29b573fe3e305f38abf51a29808718903b6cf4
Vedant Kanoje <141636261+CodeSageVedx@users.noreply.github.com>
2024-02-07 23:49:14 +0530

    Create main.py

---
+++ Day 62/main.py
@@ -0,0 +1,14 @@
+#Valid Sudoku
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        res = set()
+        for i in range(9):
+            for j in range(9):
+                element = board[i][j]
+                if element != '.':
+                    if (i, element) in res or (element, j) in res or (i // 3, j // 3, element) in res :
+                        return False
+                    res.add((i, element))
+                    res.add((element, j))
+                    res.add((i // 3, j // 3, element))
+        return True
