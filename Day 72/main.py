1edfef8b3b545f3224c4775283ec1dbe360b4441
Vedant Kanoje <141636261+CodeSageVedx@users.noreply.github.com>
2024-02-08 23:45:56 +0530

    Create main.py

---
+++ Day 72/main.py
@@ -0,0 +1,11 @@
+# Search a 2D Matrix
+class Solution:
+    def searchMatrix(self,matrix:List[List[int]],target:int)->bool:
+        n=len(matrix)
+        m=len(matrix[0])
+        for i in range(n):
+            for j in range(m):
+                if matrix[i][j]==target:
+                    return True
+
+        return False                     
